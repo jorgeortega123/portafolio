d9228d2614cd8f97e7c67fb867e1cddca9abb304
luis <luis@pop-os.localdomain>
2023-09-18 13:00:16 -0500

    add link proyect comunity and work

--- components/page/Comunity.tsx
+++ components/page/Comunity.tsx
@@ -1,8 +1,55 @@
-import { Container, H1, P, useT } from "cllk";
+import { Button, Container, H1, P, useT } from "cllk";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 function Comunity() {
   const { t } = useT();
+  const data = [
+    {
+      title: "@llampukaq/realm",
+      bg: "bg-gradient-to-r from-pink-500 to-red-500",
+      p: t(
+        "Esta es una libreria que dise;e para esta empresa, la cual mantengo y uso con frecuencia en mis proyectos, permite gestionar el backend as services de realm, de manera mas sencilla y con multiples plugins que le va a dar facilidad de uso",
+        "This is a library that I designed for this company, which I maintain and use frequently in my projects, it allows you to manage the backend as services of realm, in a simpler way and with multiple plugins that will give it ease of use"
+      ),
+      link: "https://realm.llampukaq.com/",
+    },
+    {
+      title: "@llampukaq/builder",
+      bg: "bg-gradient-to-r from-green-500 to-blue-500",
+      p: t(
+        "Un proyecto que aún está en desarrollo y que permite utilizar la biblioteca Grapesjs, potenciando y creando complementos de fácil acceso.",
+        "A project that is still in development and allows the use of the grapesjs library, enhancing and creating easy-access plugins"
+      ),
+      link: "https://www.npmjs.com/package/@llampukaq/builder",
+    },
+    {
+      title: "cllk",
+      bg: "bg-gradient-to-r from-indigo-500 to-purple-500",
+      p: t(
+        "una biblioteca de componentes esencial y fácil de usar, pretende ser ligera y fácil de usar",
+        "a essential and easy-to-use component library, it aims to be light and easy to use"
+      ),
+      link: "https://www.npmjs.com/package/cllk",
+    },
+    {
+      title: "react-languajes",
+      bg: "bg-gradient-to-br from-blue-700 via-blue-800 to-gray-900",
+      p: t(
+        "una libreria para integrar traduccion en react",
+        "una libreria para integrar traduccion en react"
+      ),
+      link: "https://www.npmjs.com/package/cllk",
+    },
+    {
+      title: "react-cache-state",
+      bg: "bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900",
+      p: t(
+        "una libreria que permite gurdar datos, persistiendo en memoria y encritando la informacion guardad",
+        "a library that allows saving data, persistence in memory and encrypting the saved information"
+      ),
+      link: "https://www.npmjs.com/package/react-cache-state",
+    },
+  ];
   return (
     <div className="w-full space-y-3">
       <H1 size="2rem">{t("Comunidad", "Comunity")} </H1>
@@ -24,24 +71,23 @@ function Comunity() {
           slidesPerView={1}
           className="mySwiper"
         >
-          <SwiperSlide className="w-full mx-auto">
-            <Container className="h-80 w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"></Container>
-          </SwiperSlide>
-          <SwiperSlide className="w-full">
-            <Container className="h-80 w-full bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></Container>
-          </SwiperSlide>
-          <SwiperSlide className="w-full">
-            <Container className="h-80 w-full bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400"></Container>
-          </SwiperSlide>
-          <SwiperSlide className="w-full">
-            <Container className="h-80 w-full bg-gradient-to-r from-indigo-300 to-purple-400"></Container>
-          </SwiperSlide>
-          <SwiperSlide className="w-full">
-            <Container className="h-80 w-full bg-gradient-to-br from-blue-700 via-blue-800 to-gray-900"></Container>
-          </SwiperSlide>
-          <SwiperSlide className="w-full">
-            <Container className="h-80 w-full bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900"></Container>
-          </SwiperSlide>
+          {data.map((x, index) => (
+            <SwiperSlide key={index} className="w-full mx-auto">
+              <Container
+                className={`h-80 w-full rounded-3xl ${x.bg} flex flex-col justify-between py-10 items-center space-y-5`}
+              >
+                <H1>{x.title}</H1>
+                <P>{x.p}</P>
+                <Button
+                  onClick={() => {
+                    window.open(x.link);
+                  }}
+                >
+                  {t("Abrir", "Open")}
+                </Button>
+              </Container>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
--- components/page/RecentlyWork.tsx
+++ components/page/RecentlyWork.tsx
@@ -1,40 +1,34 @@
-import { Button, H1, Img, P } from "cllk";
+import { Button, H1, Img, P, useT } from "cllk";
 import React from "react";
 
 type Props = {};
 
 function RecentlyWork({}: Props) {
+  const { t } = useT();
   return (
     <div className="space-y-5">
-      <H1 size="2rem">My recent work</H1>
+      <H1 size="2rem">{t("Mi trabajo reciente", "My recently work")} </H1>
       <div className="rounded-3xl bg-zinc-800 w-10/12 mx-auto">
         <Img
-          className="w-full rounded-3xl border"
+          className="w-full rounded-3xl "
           src={"/asset/llampukaq.webp"}
         ></Img>
         <div className="p-5 space-y-3">
-          <H1>Llampukaq Technology</H1>
+          <H1>Llampukaq Technology </H1>
           <P>
-            My trabajo en esta empresa consitio en mantener toda la
-            infraestructura frontend, coenctar correctamente los datos de
-            backend y mantener el rendimiento de frontend
+            {t(
+              "My trabajo en esta empresa consitio en mantener toda la infraestructura frontend, coenctar correctamente los datos de backend y mantener el rendimiento de frontend",
+              "My job at this company is to maintain the entire frontend infrastructure, correctly connect backend data, and maintain frontend performance."
+            )}
           </P>
-          <Button center>Mirar</Button>
-        </div>
-      </div>
-      <div className="rounded-3xl bg-zinc-800 w-10/12 mx-auto">
-        <Img
-          className="w-full rounded-3xl border"
-          src={"/asset/llampukaq.webp"}
-        ></Img>
-        <div className="p-5 space-y-3">
-          <H1>Llampukaq Technology</H1>
-          <P>
-            My trabajo en esta empresa consitio en mantener toda la
-            infraestructura frontend, coenctar correctamente los datos de
-            backend y mantener el rendimiento de frontend
-          </P>
-          <Button center>Mirar</Button>
+          <Button
+            onClick={() => {
+              window.open("https://llampukaq.pages.dev/");
+            }}
+            center
+          >
+            Mirar
+          </Button>
         </div>
       </div>
     </div>
