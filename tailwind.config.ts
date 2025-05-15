caa5cec1770d647db38515bc8d94197c0cc3cc74
jorgeortega123 <garridomateo74@gmail.com>
2025-05-15 15:54:33 -0500

    Agregado soporte para estilos de Hero UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/proyectos/*.{js,ts,jsx,tsx,mdx}",
     "./components/Form/*.{js,ts,jsx,tsx,mdx}",
     "./components/layout/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
