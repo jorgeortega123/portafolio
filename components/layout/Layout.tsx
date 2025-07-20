e4a54b067a6008ae96096edec42a46668382a4b2
jorgeortega123 <garridomateo74@gmail.com>
2025-07-19 23:25:46 -0500

    Metricas de vercel agregada

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -6,13 +6,14 @@ import NavBar from "../NavBar";
 import NavBarV2 from "../NavBarV2";
 import { useRouter } from "next/router";
 import { Alert } from "@heroui/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 function Layout({ children }: { children: ReactNode }) {
   const [minHeight, setMinHeight] = useState<number>(0);
   const { locale } = useRouter();
 
   return (
     <>
+      <SpeedInsights />
       <div className={` min-h-[calc(100vh_+_350px)] mb-[800px] lg:mb-[550px] `}>
         <NavBar />
 
@@ -23,7 +24,7 @@ function Layout({ children }: { children: ReactNode }) {
       <Footer />
       {locale === "en-US" && (
         <Alert
-        className="fixed bottom-3 left-3 max-w-max z-[12]"
+          className="fixed bottom-3 left-3 max-w-max z-[12]"
           title={"Incomplete translation into English"}
           description={"I'm still working on it"}
         />
